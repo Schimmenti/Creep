91f769c32a57baf542955c19f46665e337a8c9c8
Schimmenti <vinci95s@gmail.com>
2021-06-08 10:53:00 +0200

    Extract Interface

--- extract_interface.py
+++ extract_interface.py
@@ -24,11 +24,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--input')
 parser.add_argument('--output')
 parser.add_argument('--discretization', type=int, default=700)
+parser.add_argument('--initial_time',type=int, default=0)
 args = parser.parse_args()
 filename = args.input
 out_filename = args.output
-n_angles = args.discretization
+n_bins = args.discretization+1
+t0 = args.initial_time
 
 data = pd.read_csv(filename, header=None).values
 xc,yc,radius_x,radius_y = center(data)
 
+#polar coordinates
+nrow=data.shape[0]
+ncol=data.shape[1]
+xcoord = np.arange(0,ncol).reshape(1,-1)
+ycoord = np.arange(0,nrow).reshape(-1,1)
+th=np.arctan2(ycoord-yc,xcoord-xc)
+rr=np.sqrt(np.power((ycoord-yc),2) + np.power((xcoord-xc),2))
+#pandas dataframe with triples (th,R,t)
+table = pd.DataFrame(np.concatenate([th.reshape(-1,1),rr.reshape(-1,1),data.reshape(-1,1)],axis=1),columns=['th','R','t'])
+table = table[table['t'] != -1]
+table.sort_values('t', inplace=True)
+table['t'] = table['t'].values.astype('int')
+table.reset_index(inplace=True)
+table.drop('index',axis=1, inplace=True)
+max_t = int(table['t'].values.max())
+
+#discretize angles: th -> thi which is an index between 0 (-\pi) and discretization (\pi)
+bins = np.linspace(-np.pi, np.pi, n_bins)
+digits = np.digitize(table['th'], bins)-1
+table['thi'] = digits
+
+
+interface = {}  #np.zeros((max_t-t0+1,args.discretization))
+for t in range(t0, max_t+1):
+    print(t)
+    z = table[table['t'] <= t]
+    rs = z.groupby('thi')['R'].max()
+    interface[t] = np.zeros(args.discretization)
+    interface[t][rs.index] = rs.values
+np.save(filename, interface)
